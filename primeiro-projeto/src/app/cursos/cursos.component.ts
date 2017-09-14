bd0b1ed2c0fb3e9a0b4bb62c125a3aab36558396
Leonardo <leo.sjc.ti@gmail.com>
2017-09-14 15:55:00 -0300

    Aula Angular 14/09

---
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { CursosService } from './cursos.service';
+
+
+@Component({
+  selector: 'app-cursos',
+  templateUrl: './cursos.component.html',
+  styleUrls: ['./cursos.component.css']
+})
+export class CursosComponent implements OnInit {
+
+  nomePortal: string;
+
+  cursos: string [];
+
+  constructor(private cursoService: CursosService) { 
+
+    this.nomePortal = 'http://google.com';
+    
+    this.cursos = cursoService.getCursos();
+
+  }
+
+  ngOnInit() {
+  }
+
+}
