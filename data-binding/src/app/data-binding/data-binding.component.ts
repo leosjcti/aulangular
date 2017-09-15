9cd0bf6e7c04a50af68a1ad001a756fac4750e8d
Leonardo <leo.sjc.ti@gmail.com>
2017-09-15 17:41:10 -0300

    Até a aula 12 - 15/09/2017

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -7,12 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataBindingComponent implements OnInit {
 
+  valorAtual:string;
+  valorSalvo:string;
+  isMouseOver:boolean = false;
+
+  nome:string  = 'abc';
+
+  pessoa:any = {
+    nome:'Leonardo',
+    idade: "30"
+  }
 
   url:string = 'http://google.com';
   urlImagem = 'http://lorempixel.com/400/200/nature/';
 
   getValor(){
     return 1;
+  }
+
+  botaoClicado(){
+    alert("Botão Clicado");
+  }
+
+  onKeyUp(evento:KeyboardEvent){
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  salvar(valor){
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
   }
 
   constructor() { }
